f04e9c806b079d59f04c45ab9ecba6117071afb9
alessimichele <alessimichele00@yahoo.it>
2023-06-09 13:26:30 +0200

    removed self.lik, added get_log_lik_text class function

--- src/CipherBreaker.py
+++ src/CipherBreaker.py
@@ -1,6 +1,6 @@
 import random
 import math
-from src.CipherUtils import TextDecoder, TextLik
+from src.CipherUtils import TextDecoder
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 
@@ -19,17 +19,17 @@ class CipherBreaker:
         self.ciphered_text = ciphered_text
         self.probability_table = probability_table
 
-        self.decoder = TextDecoder()  # self.decoder.decode_text(text, permutated_cipher)
-        self.lik = TextLik()  # self.lik.get_log_lik_text(text, probability_table)
+        self.decoder = (
+            TextDecoder()
+        )  # self.decoder.decode_text(text, permutated_cipher)
 
-        self.history = {self.ciphered_text: self.lik.get_log_likelihood(self.ciphered_text, probability_table)}
+        self.history = {self.ciphered_text: self.get_log_likelihood(self.ciphered_text)}
 
         # self.decoded_texts = []  # List to store decoded texts
 
-        # Store maximum log-likelihood achieved 
-        #self.max_log_lik = self.lik.get_log_likelihood(self.ciphered_text, probability_table)
-        #self.max_lik_text = self.ciphered_text
-
+        # Store maximum log-likelihood achieved
+        # self.max_log_lik = self.lik.get_log_likelihood(self.ciphered_text, probability_table)
+        # self.max_lik_text = self.ciphered_text
 
     def swap(self, x):
         """
@@ -44,6 +44,24 @@ class CipherBreaker:
         rand_indices = random.sample(range(len(x)), k=2)
         x[rand_indices[0]], x[rand_indices[1]] = x[rand_indices[1]], x[rand_indices[0]]
         return x
+
+    def get_log_likelihood(self, text):
+        """
+        Calculates the log-likelihood of the given text based on a probability table.
+
+        Args:
+            text (str): The input text.
+
+        Returns:
+            float: The log-likelihood of the text.
+        """
+        two_char_list = [text[i : i + 2] for i in range(len(text) - 1)]
+        probabilities = [
+            self.probability_table.get(two_char, 1 / len(self.probability_table))
+            for two_char in two_char_list
+        ]
+        log_likelihood = sum(math.log(prob) for prob in probabilities)
+        return log_likelihood
 
     def break_cipher(self, iterations=10000, print_interval=20):
         """
@@ -67,12 +85,8 @@ class CipherBreaker:
                 self.ciphered_text, self.current_cipher
             )
 
-            proposed_log_likelihood = self.lik.get_log_likelihood(
-                decoded_text_proposed, self.probability_table
-            )
-            current_log_likelihood = self.lik.get_log_likelihood(
-                decoded_text_current, self.probability_table
-            )
+            proposed_log_likelihood = self.get_log_likelihood(decoded_text_proposed)
+            current_log_likelihood = self.get_log_likelihood(decoded_text_current)
 
             acceptance_probability = min(
                 1, math.exp(proposed_log_likelihood - current_log_likelihood)
@@ -94,19 +108,19 @@ class CipherBreaker:
                 # self.decoded_texts.append(decoded_text_proposed)  # Store decoded text
                 self.history[decoded_text_proposed] = proposed_log_likelihood
 
-
         return self.current_cipher
-    
-    
-    def extract_best(self, n_extract = 5, return_likelihood = False):
+
+    def extract_best(self, n_extract=5, return_likelihood=False):
         """
         Extracts the n_extract decoded texts with the highest log-likelihood.
         If log_lik = True it returns a list of item value pairs
         """
-        sorted_dict = dict(sorted(self.history.items(), key=lambda item: item[1], reverse = True))
+        sorted_dict = dict(
+            sorted(self.history.items(), key=lambda item: item[1], reverse=True)
+        )
         if return_likelihood == True:
             return list(sorted_dict.items())[:n_extract]
-        
+
         return list(sorted_dict.keys())[:n_extract]
 
     def generate_animation(self, filename="cipher_iterations.gif"):
