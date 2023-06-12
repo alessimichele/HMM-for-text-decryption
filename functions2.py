60d64c12ebb0d6f672885a88eda22623dbe00621
alessimichele <alessimichele00@yahoo.it>
2023-06-12 22:50:52 +0200

    FUNZIONAAAAAAAAAAAA

--- functions2.py
+++ functions2.py
@@ -1,9 +1,45 @@
 import numpy as np
 
-##### SCALING CON SAMU
+##### GIUSTO GIUSTO GIUSTO GIUSTO GIUSTO GIUTSO
 
 
 # We need an algorithm to perform belief propagation on our hmm
+"""
+def forward_HMM(A, B, pi, observed):
+    
+    A: transition
+    B: emission
+    pi: initial
+    n_nodes: number of nodes in the chain
+    observed: list containing observed ones.
+    
+    n_nodes = len(observed)
+    n_states = A.shape[0]
+    alpha = np.zeros((n_nodes, n_states))
+    c = np.zeros(n_nodes)
+    alpha_hat = np.zeros((n_nodes, n_states))
+
+    for j in range(n_states):
+        alpha[0, j] = pi[j] * B[j, observed[0]]
+
+    c[0] = np.sum(alpha[0])
+    alpha_hat[0] = alpha[0] / np.sum(alpha[0])
+    print("alpa[0]", alpha[0])
+
+    for i in range(1, n_nodes):
+        for j in range(n_states):
+            for k in range(n_states):
+                alpha[i, j] = (
+                    alpha[i, j] + A[k, j] * B[j, observed[i]] * alpha[i - 1, k]
+                )
+        print(f"alpa[{i}]", alpha[i])
+        c[i] = np.sum(alpha[i]) / np.prod(c[0:i])
+        alpha_hat[i] = alpha[i] / np.sum(alpha[i])
+
+    return alpha, alpha_hat, c
+"""
+
+
 def forward_HMM(A, B, pi, observed):
     """
     A: transition
@@ -23,27 +59,26 @@ def forward_HMM(A, B, pi, observed):
 
     c[0] = np.sum(alpha[0])
     alpha_hat[0] = alpha[0] / np.sum(alpha[0])
+    # print("alpa[0]", alpha[0])
 
     for i in range(1, n_nodes):
         for j in range(n_states):
             for k in range(n_states):
-                alpha[i, j] = (
-                    alpha[i, j] + A[k, j] * B[j, observed[i]] * alpha[i - 1, k]
-                )
-        c[i] = np.sum(alpha[i]) / np.prod(c[0:i])
-        alpha_hat[i] = alpha[i] / np.sum(alpha[i])
-
-    return alpha, alpha_hat, c
+                alpha[i, j] += A[k, j] * B[j, observed[i]] * alpha_hat[i - 1, k]
+        c[i] = np.sum(alpha[i])
+        alpha_hat[i] = alpha[i] / c[i]
+    return alpha_hat, c
 
 
+"""
 # We need an algorithm to perform belief propagation on our hmm
 def backward_HMM(A, B, observed, c):
-    """
+    
     A: transition
     B: emission
     n_nodes: number of nodes in the chain
     observed: list containing observed ones.
-    """
+    
     n_nodes = len(observed)
     n_states = A.shape[0]
     beta = np.zeros((n_nodes - 1, n_states))
@@ -64,6 +99,34 @@ def backward_HMM(A, B, observed, c):
         beta_hat[i] = beta[i] / np.prod(c[i + 1 :])
 
     return beta, beta_hat
+"""
+
+
+def backward_HMM(A, B, observed, c):
+    """
+    A: transition
+    B: emission
+    n_nodes: number of nodes in the chain
+    observed: list containing observed ones.
+    """
+    n_nodes = len(observed)
+    n_states = A.shape[0]
+    beta = np.zeros((n_nodes - 1, n_states))
+    beta_hat = np.zeros((n_nodes - 1, n_states))
+
+    for j in range(n_states):
+        for k in range(n_states):
+            beta[-1, j] += A[j, k] * B[k, observed[n_nodes - 1]]
+
+    beta_hat[-1] = beta[-1] / c[-1]
+
+    for i in range(n_nodes - 3, -1, -1):
+        for j in range(n_states):
+            for k in range(n_states):
+                beta[i, j] += A[j, k] * B[k, observed[i + 1]] * beta_hat[i + 1, k]
+        beta_hat[i] = beta[i] / c[i + 1]
+
+    return beta_hat
 
 
 def compute_all_conditional(alpha, beta):
@@ -111,8 +174,8 @@ def update_B(gamma, observed):
 def Baum_Welch(A, B_start, pi, observed, maxIter=100):
     B = np.copy(B_start)
     for it in range(maxIter):
-        alpha, alpha_hat, c = forward_HMM(A, B, pi, observed)
-        beta, beta_hat = backward_HMM(A, B, observed, c)
+        alpha_hat, c = forward_HMM(A, B, pi, observed)
+        beta_hat = backward_HMM(A, B, observed, c)
         gamma = compute_all_conditional(alpha_hat, beta_hat)
         B = update_B(gamma, observed)
 
